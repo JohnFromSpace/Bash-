49cb97d85d8883a7704f09612d4e39c87a57a2b5
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2023-01-09 22:45:55 +0200

    Update Raindrops.sh

--- Exercism/Raindrops.sh
+++ Exercism/Raindrops.sh
@@ -1,1 +1,9 @@
+#!/usr/bin/env bash
 
+raindrop=""
+
+(($1 % 3 == 0)) && raindrop+="Pling"
+(($1 % 5 == 0)) && raindrop+="Plang"
+(($1 % 7 == 0)) && raindrop+="Plong"
+
+[ -z "$raindrop" ] && echo "$1" || echo "$raindrop"
