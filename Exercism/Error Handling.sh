abcc7ffd356a573d4262843964efa088f430b0b8
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2023-01-09 22:45:14 +0200

    Update Error Handling.sh

--- Exercism/Error Handling.sh
+++ Exercism/Error Handling.sh
@@ -1,1 +1,16 @@
+#!/usr/bin/env bash
 
+usage() {
+  echo "Usage: ${0##*/} <person>"
+}
+
+main () {
+  local name="$1"
+  if [[ $# -ne 1 ]]; then
+    usage
+    return 1
+  fi
+  echo "Hello, ${name}"
+}
+
+main "$@"
